451b57870268e8405fb7be152ad78aead504551d
Raul Torres <urbanstorm@Raul-MacBook-Air.local>
2025-08-06 00:10:22 +0200

    feat: hero + global styles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,1 @@
-document.addEventListener('DOMContentLoaded',()=>{const r=document.querySelector('.ribbon');
-  window.addEventListener('scroll',()=>{window.scrollY>window.innerHeight/2?
-    r.classList.add('show-ribbon'):r.classList.remove('show-ribbon');});
-  document.getElementById('year').textContent=new Date().getFullYear();});
+document.addEventListener('DOMContentLoaded',()=>{document.getElementById('year').textContent=new Date().getFullYear();});
